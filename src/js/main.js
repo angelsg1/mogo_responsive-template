4edd9c05228b62a9db1d822c01b2618803169cf6
Angelsg1 <angel.sanchez@coherence.co.uk>
2018-02-05 12:09:24 +0000

    styling changes

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function(){
   // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+  $(".navbar a").on('click', function(event) {
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
